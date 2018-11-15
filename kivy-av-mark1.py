b3ab219ca28e53f78be253859f889724010c8936
vilerozes <vilerozes@gmail.com>
2018-11-16 00:16:20 +0100

    Add files via upload

---
+++ kivy-av-mark1.py
@@ -0,0 +1,19 @@
+import kivy
+
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.video import Video
+
+
+class MyApp(App):
+    def build(self):
+        video = Video(source='D:/w3b/python-practice/kivy-av-mark1/video-sample/test-two.mp4')
+        video.state = 'play'
+        video.options = {'eos': 'loop'}
+        video.allow_stretch = False
+        return video
+
+
+if __name__ == '__main__':
+    MyApp().run()
