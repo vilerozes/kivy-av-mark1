d123e9f68b91f9049b44e28840b8d9c4bc4e6465
vilerozes <vilerozes@gmail.com>
2018-11-16 00:25:25 +0100

    test2

--- kivy-av-mark1.py
+++ kivy-av-mark1.py
@@ -8,7 +8,7 @@ from kivy.uix.video import Video
 
 class MyApp(App):
     def build(self):
-        video = Video(source='D:/w3b/python-practice/kivy-av-mark1/video-sample/test-two.mp4')
+        video = Video(source='D:/w3b/python-practice/kivy-av-mark2/video-sample/test-two.mp4')
         video.state = 'play'
         video.options = {'eos': 'loop'}
         video.allow_stretch = False
