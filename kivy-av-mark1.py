507264192aa91bce5ed8861246e32fbcc308fdfb
vilerozes <vilerozes@gmail.com>
2018-11-16 00:27:41 +0100

    test3

--- kivy-av-mark1.py
+++ kivy-av-mark1.py
@@ -5,7 +5,6 @@ kivy.require('1.9.0')
 from kivy.app import App
 from kivy.uix.video import Video
 
-
 class MyApp(App):
     def build(self):
         video = Video(source='D:/w3b/python-practice/kivy-av-mark2/video-sample/test-two.mp4')
@@ -13,7 +12,6 @@ class MyApp(App):
         video.options = {'eos': 'loop'}
         video.allow_stretch = False
         return video
-
 
 if __name__ == '__main__':
     MyApp().run()
